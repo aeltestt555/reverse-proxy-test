ff7be58b8191718cc3457452728b7e5d7573f9cd
ali <lap.game4w@hotmail.com>
2026-08-13 14:56:13 +0100

    Initial Python app

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+APP_NAME = os.getenv("APP_NAME", "Unknown")
+APP_MESSAGE = os.getenv("APP_MESSAGE", "No message")
+
+class App(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        response = f"""
+        <h1>{APP_NAME}</h1>
+        <p>{APP_MESSAGE}</p>
+        """
+
+        self.wfile.write(response.encode())
+
+HTTPServer(("127.0.0.1", 8000), App).serve_forever()
