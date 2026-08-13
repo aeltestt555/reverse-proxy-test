65c1e3fa59e673ec8651b1268adc6cb4d83e2b92
ali <lap.game4w@hotmail.com>
2026-08-13 16:12:10 +0100

    second commi

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ class App(BaseHTTPRequestHandler):
 
         response = f"""
         <h1>{APP_NAME}</h1>
-        <p>{APP_MESSAGE}</p>
+        <p>{APP_MESSAGE} version 2</p>
         """
 
         self.wfile.write(response.encode())
