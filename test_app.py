c8828fc3b41d7fbd0db8661c512d774c8689ca11
ali <lap.game4w@hotmail.com>
2026-08-13 20:39:33 +0100

    Fix unit test

--- test_app.py
+++ test_app.py
@@ -1,2 +1,9 @@
-def test_example():
-    assert 1 + 1 == 2
+import unittest
+
+class TestApp(unittest.TestCase):
+
+    def test_example(self):
+        self.assertEqual(1 + 1, 2)
+
+if __name__ == "__main__":
+    unittest.main()
